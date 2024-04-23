0374d8910a4d3dd39aa87555df0bf4fe6e5cc414
jprtenorio00 <jp-tenorio@hotmail.com>
2024-04-23 15:47:00 -0300

    Trabalhando com Grafo

--- sqlparser.py
+++ sqlparser.py
@@ -16,7 +16,7 @@ class SQLQueryParser:
     def parse_query(self):
         parsed = sqlparse.parse(self.query)[0]
         self.tokens = [(token.ttype, token.value) for token in parsed.tokens if token.ttype]
-    
+
     def extract_conditions(self, condition_string):
         operators_pattern = re.compile(r'(\b(?:=|>|<|<=|>=|<>|AND|OR)\b|\(|\))', re.I)
         tokens = operators_pattern.split(condition_string)
@@ -43,17 +43,53 @@ class SQLQueryParser:
         if where_match:
             components['WHERE'] = self.extract_conditions(where_match.group(1).strip())
 
-        joins = re.findall(r'JOIN(.*?)ON', self.query, re.S | re.I)
+        joins = re.findall(r'JOIN(.*?)(?=JOIN|$)', self.query, re.S | re.I)
         for join in joins:
             join_components = {}
-            join_components['table'] = join.strip()
-            on_match = re.search(r'ON(.*?)$', join, re.S | re.I)
-            if on_match:
-                join_components['on'] = self.extract_conditions(on_match.group(1).strip())
+            join_components['table'], *on_clause = re.split(r'ON', join, 1, flags=re.I)
+            join_components['table'] = join_components['table'].strip()
+            if on_clause:
+                join_components['on'] = self.extract_conditions(on_clause[0].strip())
+            else:
+                join_components['on'] = []
             components['JOIN'].append(join_components)
 
         return components
-    
+
+class SQLGraph:
+    def __init__(self, components):
+        self.graph = nx.DiGraph()
+        self.build_graph(components)
+
+    def build_graph(self, components):
+        for sel in components['SELECT']:
+            self.graph.add_node(sel.strip(), label='SELECT')
+        for frm in components['FROM']:
+            self.graph.add_node(frm.strip(), label='FROM')
+        for whr in components['WHERE']:
+            self.graph.add_node(whr, label='WHERE')
+        for join in components['JOIN']:
+            self.graph.add_node(join['table'], label='JOIN')
+            for cond in join['on']:
+                self.graph.add_node(cond, label='ON')
+                self.graph.add_edge(join['table'], cond)
+
+        # Adding edges based on the components and their relationships
+        if components['WHERE']:
+            for node in self.graph.nodes(data=True):
+                if node[1]['label'] == 'FROM':
+                    for whr in components['WHERE']:
+                        self.graph.add_edge(node[0], whr)
+        if components['JOIN']:
+            for join in components['JOIN']:
+                self.graph.add_edge(join['table'], join['on'][0])
+
+    def draw_graph(self):
+        pos = nx.spring_layout(self.graph)
+        labels = {node: node for node in self.graph.nodes()}
+        nx.draw(self.graph, pos, labels=labels, with_labels=True)
+        plt.show()
+
 class SQLProcessorApp:
     def __init__(self, root):
         self.root = root
@@ -70,10 +106,13 @@ class SQLProcessorApp:
         if sql:
             formatted_sql = sqlparse.format(sql, reindent=True, keyword_case='upper')
             print("Formatted SQL:", formatted_sql)
+            parser = SQLQueryParser(formatted_sql)
+            components = parser.extract_components()
+            graph = SQLGraph(components)
+            graph.draw_graph()
             self.execute_query(formatted_sql)
         else:
             messagebox.showinfo("Informação", "Por favor, insira uma consulta SQL válida.")
-
 
     def execute_query(self, sql):
         try:
